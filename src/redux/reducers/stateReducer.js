d865967ba120eb8bec85c338a08e199650b50f20
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-18 18:35:33 +0100

    ok con commenti

--- src/redux/reducers/stateReducer.js
+++ src/redux/reducers/stateReducer.js
@@ -1,3 +1,4 @@
+//slice reducer
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -18,3 +19,19 @@ const stateReducerSlice = createSlice({
 // Esporto solo l'azione definita nello slice
 export const { setFirstState } = stateReducerSlice.actions;
 export default stateReducerSlice.reducer;
+
+//vanilla reducer
+/* export const FETCH_JOBS = "FETCH_JOBS";
+
+export const fetchJobsAction = (url, query) => async (dispatch) => {
+  try {
+    const response = await fetch(url + query + "&limit=20");
+
+    if (response.ok) {
+      const { data } = await response.json();
+      dispatch({ type: FETCH_JOBS, payload: data });
+    } else {
+      throw new Error("Errore nel recupero dei risultati");
+    }
+  } catch (error) {}
+}; */
