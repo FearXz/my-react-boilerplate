f987db06e452a55f7c62f9064997216b4b379218
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-24 18:38:37 +0100

    ok

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function MyMain() {
   return (
     <Container>
       <Row>
         <Col className=" d-flex justify-content-center">
-          <div className=" fs-1 text-danger bg-darkgray">MEGA KEBAB</div>
+          <div className=" fs-1 text-danger  bg-gray-900">MEGA KEBAB</div>
         </Col>
       </Row>
+      <Button className=" btn-blue-500"> BOTTONE KEBAB</Button>
+      <Button className=" btn-blue-600"> BOTTONE KEBAB</Button>
+      <Button className=" btn-blue-700"> BOTTONE KEBAB</Button>
+      <Button className=" btn-blue-800"> BOTTONE KEBAB</Button>
+      <Button className=" btn-red-500"> BOTTONE KEBAB</Button>
+      <Button className=" btn-red-600"> BOTTONE KEBAB</Button>
+      <Button className=" btn-red-700"> BOTTONE KEBAB</Button>
+      <Button className=" btn-red-800"> BOTTONE KEBAB</Button>
     </Container>
   );
 }
